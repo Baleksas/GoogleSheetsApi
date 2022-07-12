7a2dd71ef23317bb4b4bc9677acd1ff96535eff7
Baleksas <alexasbag17@gmail.com>
2022-07-12 14:16:15 +0100

    feat: start to implement functions chain function which will make the steps in order to replicate sheet and insert custom data

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const readline = require("readline");
 const { google } = require("googleapis");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -14,10 +14,13 @@ fs.readFile("client-secret.json", (err, content) => {
   if (err) return console.log("Error loading client secret file:", err);
   // Authorize a client with credentials, then call the Google Sheets API.\
 
+  authorize(JSON.parse(content), functionsChain);
+
   // authorize(JSON.parse(content), getValues);
   // authorize(JSON.parse(content), getValuesMultipleRanges);
   // authorize(JSON.parse(content), writeValues);
-  authorize(JSON.parse(content), create);
+  // authorize(JSON.parse(content), createSheet);
+  // authorize(JSON.parse(content), copySheet);
 });
 
 /**
@@ -107,12 +110,26 @@ const paramsCreate = {
   title: `SpreadSheet${Math.random(0, 100)}`,
 };
 
-/**
- * Create a google spreadsheet
- * @param {string} title Spreadsheets title
- * @return {string} Created spreadsheets ID
- */
-async function create(auth) {
+// Makes a copy of a specified sheet. Important: destination sheet id must exist before copying into it.
+function copySheet(auth) {
+  const sheets = google.sheets({ version: "v4", auth });
+  const request = {
+    spreadsheetId: "1Znc2RBemy_rvsBZXv2EwDItin4e76Vp3nM3iWv_QqKw",
+    sheetId: 1805430215,
+    resource: {
+      destinationSpreadsheetId: "1Znc2RBemy_rvsBZXv2EwDItin4e76Vp3nM3iWv_ABCD",
+    },
+  };
+
+  sheets.spreadsheets.sheets.copyTo(request, (err, res) => {
+    console.log(res);
+    if (err) {
+      console.log("Error: ", err);
+    }
+  });
+}
+
+function createSheet(auth) {
   const sheets = google.sheets({ version: "v4", auth });
   const request = {
     resource: {
@@ -130,11 +147,6 @@ async function create(auth) {
   });
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
 function getValues(auth) {
   const sheets = google.sheets({ version: "v4", auth });
   sheets.spreadsheets.values.get(params, (err, res) => {
@@ -163,6 +175,7 @@ function getValuesMultipleRanges(auth) {
     });
   });
 }
+
 function writeValues(auth) {
   const sheets = google.sheets({ version: "v4", auth });
   sheets.spreadsheets.values.update(paramsWrite, (err, res) => {
@@ -171,4 +184,31 @@ function writeValues(auth) {
     console.log(res);
     console.log("res: ", res);
   });
+}
+
+// Create sheet
+// Get sheet id and spreadsheet id
+// Copy from the original copy of the timesheet to a created sheet by providing sheet id and spreadsheetid (give it the dynamic title as well)
+// Pick cells or form a 2d array to dynamically change values of the specific cells
+// Write into the copy spreadsheet 2d arrays with dynamically written arguments to change specific cells
+
+// QUESTIONS
+// How to make functions with arguments. Need to find a way around auth by implementing it inside every function?
+// If above question is solved, UI could be made in order to dynamically write values and make requests by buttons
+
+async function functionsChain(auth) {
+  const sheets = google.sheets({ version: "v4", auth });
+  // Create
+  const request = {
+    resource: {
+      properties: {
+        title: "TestingSheettttt",
+      },
+    },
+  };
+  try {
+    const createRes = await sheets.spreadsheets.create(request);
+  } catch (error) {
+    console.log(error);
+  }
 }
